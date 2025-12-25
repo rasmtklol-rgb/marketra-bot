72a6b6238d58dc860ce6e1ba62177d5e75eabc57
rasmtklol-rgb <rasmtklol@gmail.com>
2025-12-25 14:25:30 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models import Base
 
+engine=create_engine("sqlite:///economy.db")
+Session=sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
