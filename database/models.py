465b2a41b76fbaeaf62bfedab3e82c45778e96a9
rasmtklol-rgb <rasmtklol@gmail.com>
2025-12-25 14:26:14 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,1 +1,15 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer,String
 
+Base = declarative_base()
+
+class Subscription(Base):
+    __tablename__="subscriptions"
+    guild_id=Column(Integer,primary_key=True)
+    expires_at=Column(Integer)
+    grace_until=Column(Integer)
+
+class GuildSettings(Base):
+    __tablename__="guild_settings"
+    guild_id=Column(Integer,primary_key=True)
+    prefix=Column(String,default="!")
